3ee5880c201c72bb99d0be4afcccebb9fb6cc57e
DoubleDew <mdeaconu42@gmail.com>
2024-11-04 10:41:56 +0200

    Finished lab + bonus

--- Python Training/Python 101/Lab 3/Bonus_Skel/task01.py
+++ Python Training/Python 101/Lab 3/Bonus_Skel/task01.py
@@ -2,20 +2,35 @@
 class Complex:
 
     def __init__(self, real_part: int = 0, imaginary_part: int = 0, var_name: str = 'var') -> None:
-        pass
+        self.real_part = real_part
+        self.imaginary_part = imaginary_part
+        self.var_name = var_name
 
     def __str__(self) -> str:
-        pass
+        sign = '+' if self.imaginary_part >= 0 else '-'
+        
+        if self.imaginary_part == 0:
+            return f'{self.var_name} = {self.real_part}'
+        
+        if self.real_part == 0:
+            return f'{self.var_name} = {self.imaginary_part}i'
+        
+        return f'{self.var_name} = {self.real_part} {sign} {abs(self.imaginary_part)}i'
 
     def add_complex_numbers(self, other) -> None:
-        pass
+        self.real_part += other.real_part
+        self.imaginary_part += other.imaginary_part
+        
 
     def substract_complex_numbers(self, other) -> None:
-        pass
+        self.real_part -= other.real_part
+        self.imaginary_part -= other.imaginary_part
+        
 
     def multiply_complex_numbers(self, other) -> None:
+        x = self.real_part * other.real_part - self.imaginary_part * other.imaginary_part
+        y = self.real_part * other.imaginary_part + self.imaginary_part * other.real_part
+        self.real_part = x
+        self.imaginary_part = y
         
-        """
-        We know you all love Math. :)
-        """
-        pass
+       

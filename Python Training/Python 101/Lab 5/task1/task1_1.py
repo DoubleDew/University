5beff1b41aa4240b77b7eb17093e6d160758876b
DoubleDew <mdeaconu42@gmail.com>
2024-12-03 09:09:27 +0200

    Attempted Homework (not complete)

--- Python Training/Python 101/Lab 5/task1/task1_1.py
+++ Python Training/Python 101/Lab 5/task1/task1_1.py
@@ -1,13 +1,26 @@
-import requests
+import requests as request
 from bs4 import BeautifulSoup
 
 def student_function():
     url = "https://quotes.toscrape.com/"
-    # HINT 1: Use requests to get the contents of the page and BeautifulSoup to navigate it.
+    r = request.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
     dict = {}
     return scrape_page(url, soup, dict)
 
 def scrape_page(url, soup, dict):
-    # TODO: Scrape the page!
-    # HINT 2: Use the 'zip' function to go through the quotes list and the authors list at the same time.
+    
+    author = soup.find_all(itemprop = 'author')
+    quote = soup.find_all(itemprop = 'text')
+    
+    for i, j in zip(author, quote):
+        authors = i.text.strip()
+        quotes = j.text.strip()
+        quotes = quotes.replace("“", "").replace("”", "").strip()
+                                
+        if authors not in dict:
+            dict[authors] = []
+            
+        dict[authors].append(quotes)
+    
     return dict
